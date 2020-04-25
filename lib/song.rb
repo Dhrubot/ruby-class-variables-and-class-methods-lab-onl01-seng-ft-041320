ca8450d9870757aff0952b634716f5597058fe34
Dhrubo Talukder <dhrubo.dh@gmail.com>
2020-04-25 06:35:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,10 @@
 class Song 
-  attr_reader :name, :artist, :genre
+  attr_accessor :name, :artist, :genre
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
-    @@count 
+    @@count += 1
   end
 end
